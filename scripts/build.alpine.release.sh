23774fbb89deff24ff0b1fe5aff36b3867484f66
Steven Hangger <LonghronShen@Hotmail.com>
2022-12-10 08:49:06 +0000

    ci: *
    *

--- scripts/build.alpine.release.sh
+++ scripts/build.alpine.release.sh
@@ -4,7 +4,7 @@ set -xe
 
 ALPINE_VER="$(cat /etc/os-release | grep VERSION | cut -d = -f 2)"
 
-apk add --no-cache --virtual .build-deps libevent-dev pcre2-dev boost-dev icu-dev openssl-dev python3 ninja && \
+apk add --no-cache --virtual .build-deps git gcc g++ libevent-dev pcre2-dev boost-dev icu-dev openssl-dev curl-dev python3 ninja && \
     python3 -m pip install --upgrade pip && python3 -m pip install cmake
 
 bash ./build.sh
