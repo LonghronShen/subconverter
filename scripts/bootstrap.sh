1e06ff1aea4c5bb96abcd284a6f67df2c08afd53
Steven Hangger <LonghronShen@Hotmail.com>
2022-12-12 00:25:14 +0000

    fix: *

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -7,7 +7,24 @@ if [ "$EUID" -ne 0 ]
     exit
 fi
 
-pip3 install cmake cmake_format
-
 apt update
-apt install -y libboost-all-dev gcc-8 g++-8 g++-8-multilib
+apt install -y git build-essential gcc-8 g++-8 libboost-all-dev python3-pip libevent-dev libcurl4-openssl-dev libpcre2-dev ninja-build pkg-config
+
+python3 -m pip install --upgrade pip
+PIP_ONLY_BINARY=cmake python3 -m pip install cmake || true
+
+hash cmake 2>/dev/null || {
+    echo "Build CMake from source ..."
+    cd /tmp
+    git clone -b 'v3.25.1' --single-branch --depth 1 https://github.com/Kitware/CMake.git CMake
+    cd CMake
+    ./bootstrap --prefix=/usr/local
+    make -j$(nproc)
+    make install
+    cd ..
+    rm -rf CMake
+}
+
+cmake --version
+
+curl -sSL https://raw.githubusercontent.com/nektos/act/master/install.sh | bash /dev/stdin -b /usr/local/bin
