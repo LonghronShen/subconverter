7870d58286cf7e0bb22a96c65ae6ea22bd293a5b
LonghronShen <LonghronShen@hotmail.com>
2022-12-15 23:48:36 +0800

    chore(*): *

--- external/patches/jinja2cpp/thirdparty/thirdparty-external-as-target.cmake
+++ external/patches/jinja2cpp/thirdparty/thirdparty-external-as-target.cmake
@@ -1,15 +1,9 @@
 message(STATUS "'external-as-target' dependencies mode selected for Jinja2Cpp. All dependencies are used as targets.")
 
 add_subdirectory(thirdparty/nonstd/expected-lite EXCLUDE_FROM_ALL)
-
 add_subdirectory(thirdparty/nonstd/variant-lite EXCLUDE_FROM_ALL)
-add_library(variant-lite ALIAS variant-lite)
-
 add_subdirectory(thirdparty/nonstd/optional-lite EXCLUDE_FROM_ALL)
-add_library(optional-lite ALIAS optional-lite)
-
 add_subdirectory(thirdparty/nonstd/string-view-lite EXCLUDE_FROM_ALL)
-add_library(string-view-lite ALIAS string-view-lite)
 
 include (./thirdparty/external_boost_deps.cmake)
 
